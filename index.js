c648ff62ff6ce22815d2f984832aa769f507626b
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-03-01 09:17:14 +0600

    dynamic date added

--- index.js
+++ index.js
@@ -227,7 +227,19 @@ document.getElementById("btn-card6").addEventListener("click", function () {
              You have completed the task ${cardTitle} at ${formattedTime}
              `;
     activityLog.appendChild(createP);
+    alert('Congrates!!! You have completed at the current task');
+   
   } else {
     console.log("no");
   }
 });
+
+
+// activity button style 
+document.getElementById('btn-history').addEventListener('click', function(){
+    
+    document.getElementById('activity-log').style.display = 'none';
+})
+
+
+document.getElementById('date').innerText = new Date().toDateString();
